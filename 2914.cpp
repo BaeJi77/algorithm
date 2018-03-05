5c1ba43dc1260961d354a92d32b163866b598633
Ji Hun Bae <hahawjstk@gmail.com>
2018-03-06 00:15:11 +0900

    18.03.05

---
+++ 2914.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int song=38;
+    int aver=24;
+    int mel;
+    // cin>>song>>aver;
+    mel = song*(aver-1);
+    
+    cout<<mel;
+    return 0;
+}
