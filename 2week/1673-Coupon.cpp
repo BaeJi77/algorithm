98bf5d42c9a3550a659d7bcdcf883099d7d7232c
{Baeji77} <{hahawjstk@gmail.com}>
2018-03-12 21:59:04 +0900

    Coupon

--- 2week/1673-Coupon.cpp
+++ 2week/1673-Coupon.cpp
@@ -3,17 +3,21 @@
 using namespace std;
 
 int main(){
-	int Total,n;
-	cin>>Total>>n;
-	n=n+Total;
-	while(1){
-		int AddChi=n/4;
-		Total=Total+AddChi;
-		n=n%4;
-		n=n+AddChi;
-		if(n<4){
-			break;
+	// freopen("input.txt", "r", stdin);
+	int Total=0,n=0,stemp=0;
+
+	while(cin>>Total>>n){
+		stemp = Total;
+		int AddChi = 0;
+		while(1){
+			AddChi=stemp/n;
+			Total=Total+AddChi;
+			stemp=stemp%n;
+			stemp=stemp+AddChi;
+			if(stemp<n){
+				break;
+			}
 		}
+		cout<<Total<<endl;
 	}
-	cout<<Total;
 }
