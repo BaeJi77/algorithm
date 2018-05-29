69999cddb2ea2c15985faa5ff38a95665dfa7e3e
{Baeji77} <{hahawjstk@gmail.com}>
2018-05-29 13:27:50 +0900

    18.05.29

---
+++ CTP/2018_IUPC/2018_IUPC/B.cpp
@@ -0,0 +1,23 @@
+#include <cstdio>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int n, k, temp;
+int Nmaxx;
+int Kmaxx;
+int main() {
+    scanf("%d %d", &n, &k);
+    while (n--)
+    {
+        scanf("%d", &temp);
+        Nmaxx = max(Nmaxx, temp);
+    }
+    while (k--)
+    {
+        scanf("%d", &temp);
+        Kmaxx = max(Kmaxx, temp);
+    }
+    
+    printf("%d", Kmaxx + Nmaxx);
+}
--- CTP/9week/9week/2606.cpp
+++ CTP/9week/9week/2606.cpp
@@ -1,30 +1,34 @@
-#include <cstdio>
-#include <cstring>
-#include <vector>
-#include <queue>
-#include <algorithm>
-
-using namespace std;
-int n,m,v,x,y;
-vector <vector <int> > vt;
-bool visited[10004];
-
-queue <int> qu;
-bool discover[10004];
-
-
-
-int main(){
-	freopen("input.txt" , "r" , stdin);
-	scanf("%d" , &n);
-	scanf("%d" , %m);
-	while(m--){
-		scanf("%d%d" , &x, &y);
-		vt[x].push_back(y);
-	}
-
-	for(int i = 0 ; i < n ; i++){
-		
-	}
-	
-}
+//#include <cstdio>
+//#include <cstring>
+//#include <vector>
+//#include <algorithm>
+//
+//using namespace std;
+//int n,m,v,x,y;
+//vector <vector <int> > vt;
+//bool visited[10004];
+//int cnt;
+//
+//void dfs(int here){
+//    visited[here] = true;
+//    if(here != 1) cnt++;
+//    for(int i = 0 ; i < vt[here].size() ; i++){
+//        int there = vt[here][i];
+//        if(visited[there] == false) dfs(there);
+//    }
+//}
+//
+//int main(){
+//    scanf("%d" , &n);
+//    scanf("%d" , &m);
+//    vt.resize(n+1);
+//    while(m--){
+//        scanf("%d%d" , &x, &y);
+//        vt[x].push_back(y);
+//        vt[y].push_back(x);
+//    }
+//    
+//    dfs(1);
+//    printf("%d" , cnt);
+//}
+//
