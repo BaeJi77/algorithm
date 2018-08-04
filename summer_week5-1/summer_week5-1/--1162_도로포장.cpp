9ae9acc9df963dccc33c74c4ee369a4efb192d36
Baeji77 <hahawjstk@gmail.com>
2018-08-04 22:38:50 +0900

    18.08.04

--- summer_week5-1/summer_week5-1/--1162_도로포장.cpp
+++ summer_week5-1/summer_week5-1/--1162_도로포장.cpp
@@ -1,30 +1,65 @@
-////
-////  1162_도로포장.cpp
-////  summer_week5-1
-////
-////  Created by Hoon on 2018. 8. 2..
-////  Copyright © 2018년 Hoon. All rights reserved.
-////
 //
-//#include <stdio.h>
-//#include <vector>
-//#include <queue>
-//#include <cstring>
-//using namespace std;
+//  1162_도로포장.cpp
+//  summer_week5-1
 //
-//int v,e,k;
-//vector<vector<pair<int, int>>> vt;
-//priority_queue<pair<int, int>> pq;
+//  Created by Hoon on 2018. 8. 2..
+//  Copyright © 2018년 Hoon. All rights reserved.
 //
-//int main(){
-//    scanf("%d %d %d" , &v,&e,&k);
-//    for (int i = 0 ; i < e; i++) {
-//        int a,b,c;
-//        scanf("%d %d %d", &a,&b,&c);
-//        vt[a].push_back({b,c});
-//        vt[b].push_back({a,c});
-//    }
-//
-//
-//}
+
+#include <stdio.h>
+#include <vector>
+#include <queue>
+#include <cstring>
+#include <algorithm>
+#define INF 987654321
+using namespace std;
+
+typedef long long ll;
+
+ll v,e,k,minn;
+ll dist[10004][24];
+vector<vector<pair<ll, ll>>> vt;
+priority_queue<pair<ll, pair<ll, ll>>> pq;
+
+void dij(){
+    pq.push({0,{1,k}});
+    while (!pq.empty()) {
+        ll cost = -pq.top().first;
+        ll here = pq.top().second.first;
+        ll road = pq.top().second.second;
+        pq.pop();
+        
+        if(dist[here][road] != -1) continue;
+        dist[here][road] = cost;
+        
+        for (ll i = 0 ; i < vt[here].size(); i++) {
+            ll ncost = vt[here][i].second + cost;
+            ll there = vt[here][i].first;
+            
+            if(k > 0 && dist[there][k-1] == -1) pq.push({-cost,{there,road-1}});
+            if(dist[there][k] == -1) pq.push({-ncost,{there,road}});
+        }
+    }
+}
+
+int main(){
+    scanf("%lld %lld %lld" , &v,&e,&k);
+    vt.resize(v+1);
+    for (ll i = 0; i < 10004; i++) memset(dist[i],-1,sizeof(dist[i]));
+    for (ll i = 0 ; i < e; i++) {
+        ll a,b,c;
+        scanf("%lld %lld %lld", &a,&b,&c);
+        vt[a].push_back({b,c});
+        vt[b].push_back({a,c});
+    }
+    
+    dij();
+    
+    minn = INF;
+    for (ll i = 0; i <= k; i++) {
+        minn = min(dist[v][i],minn);
+    }
+    
+    printf("%lld" , minn);
+}
 
--- summer_week5-1/summer_week5-1/--14431_소수마을.cpp
+++ summer_week5-1/summer_week5-1/--14431_소수마을.cpp
@@ -1,23 +1,40 @@
-//  https://www.acmicpc.net/problem/14431
-//  14431_소수마을.cpp
-//  summer_week5-1
+////  https://www.acmicpc.net/problem/14431
+////  14431_소수마을.cpp
+////  summer_week5-1
+////
+////  Created by Hoon on 2018. 7. 31..
+////  Copyright © 2018년 Hoon. All rights reserved.
+////
 //
-//  Created by Hoon on 2018. 7. 31..
-//  Copyright © 2018년 Hoon. All rights reserved.
+//#include <stdio.h>
+//#include <vector>
+//#include <queue>
+//#include <cstring>
+//using namespace std;
 //
-
-#include <stdio.h>
-#include <vector>
-using namespace std;
-
-int sx,sy,ex,ey,n;
-
-int main(){
-    scanf("%d %d %d %d" , &sx,&sy,&ex,&ey);
-    scanf("%d" , &n);
-    for (int i = 0; i < n; i++) {
-        <#statements#>
-    }
-    
-}
-
+//int sx,sy,ex,ey,n;
+//int dist[4004][4004];
+//bool p[9000004];
+//vector<pair<int, int>> vt;
+//priority_queue<pair<int, int>> pq;
+//
+//void init(){
+//    p[0]
+//    for (int i = 2; <#condition#>; <#increment#>) {
+//        <#statements#>
+//    }
+//}
+//
+//
+//int main(){
+//    scanf("%d %d %d %d" , &sx,&sy,&ex,&ey);
+//    scanf("%d" , &n);
+//    for (int i = 0; i < n; i++) {
+//        int x,y;
+//        scanf("%d %d" , &x,&y);
+//        vt.push_back({x,y});
+//    }
+//
+//
+//}
+//
