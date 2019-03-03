fe5b875fd1fbe079a74406305a4fbef26e0bd384
BaeJi77 <hahawjstk@gmail.com>
2019-03-03 12:18:46 +0900

    git commit

---
+++ 2018_winter/KOI_지역예선/2494_숫자맞추기.cpp
@@ -0,0 +1,15 @@
+#include <cstdio>
+
+using namespace std;
+
+int N,A,B;
+
+int main(){
+    freopen("input.txt","r",stdin);
+    scanf("%d" , &N);
+    scanf("%d" , &A);
+    scanf("%d" , &B);
+    for(int i = N-1 , i >= 0 ; i++){
+        
+    }
+}
