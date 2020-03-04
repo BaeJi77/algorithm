6d1ca1eb17643801e7a466ebb16681d1a92e9f3f
Baeji77 <hahawjstk@gmail.com>
2020-03-04 11:20:36 +0900

    BOJ 1008

---
+++ BOJ/1000~4999/1008.cpp
@@ -0,0 +1,15 @@
+#include <cstdio>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+double a,b;
+
+int main() {
+    freopen("/Users/baejihoon/Desktop/algorithm/input.txt","r",stdin);
+    scanf("%lf %lf", &a, &b);
+    printf("%.9f", a/b);
+}
+
+// 0.33333333333333333333333333333333
