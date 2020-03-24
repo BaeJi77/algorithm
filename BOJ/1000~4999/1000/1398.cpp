31412f47d27fa2ac8669c6e1d60ab508702b527d
Baeji77 <hahawjstk@gmail.com>
2020-03-24 17:46:01 +0900

    BOJ 1398

---
+++ BOJ/1000~4999/1000/1398.cpp
@@ -0,0 +1,50 @@
+#include <cstdio>
+#include <algorithm>
+#include <vector>
+#include <cstring>
+
+using namespace std;
+typedef long long ll;
+const int MAX = 987654321;
+
+ll N;
+int dp[101];
+int arr[] = {1, 10, 25};
+
+int solve(int don, int goal) {
+    if(don == goal) {
+        return 0;
+    }
+
+    int& ret = dp[don];
+    if(ret != MAX)
+        return ret;
+    
+    for(int i = 0 ; i < 3 ; i++) {
+        if(don + arr[i] <=  goal)
+            ret = min(ret, solve(don + arr[i], goal) + 1);
+    }
+
+    return ret;
+}
+
+// 동전마다 쓰거나 안쓰거나 이런 개념이 아님. 
+// 1, 10, 25 / 100 1000 2500 / ... => 각 단계별로 사용되는 동전이 똑같음. 
+// 그래서 계속 레벨을 작은 단위로 낮춰서 필요한 동전 수를 추가함.
+int main() {
+    freopen("/Users/baejihoon/Desktop/algorithm/input.txt","r",stdin);
+    ll t;
+    scanf("%lld", &t);
+    while(t--) {
+        scanf("%lld", &N);
+        ll ans = 0;
+        while(1) {
+            for(int i = 0 ; i < 101; i++)
+                dp[i] = MAX;
+            if(N == 0) break;
+            ans += solve(0, N % 100);
+            N /= 100;
+        }
+        printf("%lld\n", ans);
+    }
+}
