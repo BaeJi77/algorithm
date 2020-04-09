2db9dbf27a9102ac79dbdc6173d4ec1219132ab8
Baeji77 <hahawjstk@gmail.com>
2020-04-09 23:00:34 +0900

    BOJ 1205

---
+++ BOJ/1000~4999/1000/1205.cpp
@@ -0,0 +1,12 @@
+#include <cstdio>
+#include <algorithm>
+#include <cmath>
+#include <cstring>
+#include <queue>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    freopen("/Users/baejihoon/Desktop/algorithm/input.txt","r",stdin);
+}
