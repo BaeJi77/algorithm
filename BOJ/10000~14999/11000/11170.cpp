c2484eaaf00361f0fe1657db904700e283baa602
Baeji77 <hahawjstk@gmail.com>
2020-04-10 12:55:47 +0900

    BOJ 11170

--- BOJ/10000~14999/11000/11170.cpp
+++ BOJ/10000~14999/11000/11170.cpp
@@ -9,14 +9,24 @@ using namespace std;
 
 int pre[1000010];
 
+int countZero(int number) {
+    int cnt = 0;
+    while(number != 0) {
+        if(number % 10 == 0) cnt++;
+        number /= 10;
+    }
+    return cnt;
+}
 
 void init() {
-    int cnt = 0;
-    int number = 0;
+    pre[0] = 1;
+    int number = 1;
     while(1) {
         if(number == 1000001)
             break;
         
+        pre[number] = countZero(number);
+        number++;
     }
 }
 
@@ -28,5 +38,11 @@ int main() {
     scanf("%d", &t);
     while(t--) {
         int a, b;
+        scanf("%d %d", &a, &b);
+        int cnt = 0;
+        for(int i = a ; i <= b ; i++) {
+            cnt += pre[i];
+        }
+        printf("%d\n", cnt);
     }
 }
