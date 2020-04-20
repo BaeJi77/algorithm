85377d74ba712a91fb249fbc4fc807572d0d03d1
Baeji77 <hahawjstk@gmail.com>
2020-04-21 00:23:30 +0900

    BOJ 15649

---
+++ BOJ/15000~/15649.cpp
@@ -0,0 +1,34 @@
+#include <cstdio>
+#include <algorithm>
+#include <cmath>
+#include <cstring>
+#include <queue>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int N, M;
+
+void solve(vector<int> numbers, int bit) {
+    if(numbers.size() == M) {
+        for(auto val : numbers)
+            printf("%d ", val);
+        puts("");
+        return;
+    }
+
+    for(int i = 1 ; i <= N ; ++i) {
+        if(bit & 1<<i) continue;
+        numbers.push_back(i);
+        solve(numbers, bit | 1<<i);
+        numbers.pop_back();
+    }
+}
+
+int main() {
+    freopen("/Users/baejihoon/Desktop/PS/algorithm/input.txt","r",stdin);
+    scanf("%d %d", &N, &M);
+    vector<int> temp;
+    solve(temp, 0);
+}
