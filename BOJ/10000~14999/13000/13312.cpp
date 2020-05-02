610dd3c8b382219f49b9a0e8ef9305721b02caf3
Baeji77 <hahawjstk@gmail.com>
2020-05-02 16:33:37 +0900

    BOJ 13312

---
+++ BOJ/10000~14999/13000/13312.cpp
@@ -0,0 +1,17 @@
+#include <cstdio>
+#include <iostream>
+#include <cmath>
+#include <cstring>
+#include <algorithm>
+
+#include <vector>
+#include <queue>
+#include <string>
+
+using namespace std;
+
+int main() {
+    printf("100\n");
+    for(int i  = 0 ; i < 100 ; ++i)
+        printf("1\n");
+}
