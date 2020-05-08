d26b233b9bafa201be8cd8d68a081161ee13859c
Baeji77 <hahawjstk@gmail.com>
2020-05-08 14:33:28 +0900

    BOJ 9935

---
+++ BOJ/5000~10000/9000/9935.cpp
@@ -0,0 +1,45 @@
+#include <cstdio>
+#include <iostream>
+#include <cmath>
+#include <cstring>
+#include <algorithm>
+
+#include <vector>
+#include <queue>
+#include <string>
+#include <stack>
+
+using namespace std;
+
+string input;
+string bomb;
+stack<pair<char, int> > st;
+
+int main() {
+    freopen("/Users/baejihoon/Desktop/PS/algorithm/input.txt","r",stdin);
+    cin>>input>>bomb;
+    int nowCorrectSize = 0;
+    for(int i = 0 ; i < input.size() ; ++i) {
+        if(input[i] == bomb[nowCorrectSize]) nowCorrectSize++;
+        else if(input[i] == bomb[0]) nowCorrectSize = 1;
+        else nowCorrectSize = 0;
+        
+        st.push(make_pair(input[i], nowCorrectSize));
+        // cout<<input[i]<<" "<<nowCorrectSize<<"\n";
+        if(nowCorrectSize == bomb.size()) {
+            for(int i = 0 ; i < bomb.size() ; ++i)
+                st.pop();
+            if(st.empty()) nowCorrectSize = 0;
+            else nowCorrectSize = st.top().second;
+        }
+    }
+
+    string ans = "";
+    while(!st.empty()) {
+        ans += st.top().first;
+        st.pop();
+    }
+    reverse(ans.begin(), ans.end());
+    if(ans.empty()) cout<<"FRULA";
+    else cout<<ans;
+}
